096734fc2d99926ea9ac2496479f0dee24cb4f51
vinodkpasi <vinodkpasi@gmail.com>
2022-08-13 23:50:59 +0530

    Update iframe.spec.cy.ts

--- cypress/e2e/specs/miscellaneous/iframe.spec.cy.ts
+++ cypress/e2e/specs/miscellaneous/iframe.spec.cy.ts
@@ -3,11 +3,12 @@ describe("IFrame suite", () => {
     // launch URL
     cy.visit("/iframe");
 
-    // Validate default text
+    // Validate default text inside iframe
     cy.switchToIframe("#mce_0_ifr").within(function () {
       cy.get("p").should("have.text", "Your content goes here.").clear();
     });
 
+    // Validate header text in the main page
     cy.get("h3")
       .contains("An iFrame containing the TinyMCE WYSIWYG Editor")
       .should("be.visible");
